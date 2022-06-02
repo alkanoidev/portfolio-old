b61cabcf045d2cf6552b54b22c78f6883b2f33f1
Filip Ivanovic <alkanoidev@gmail.com>
2022-06-02 12:58:38 +0200

    [chore] project initialized

---
+++ TODO.md
@@ -0,0 +1,8 @@
+# TODO
+## tech stack
+- react
+- typescript
+- redux
+- vite
+- sass
+- [blob shape](https://www.npmjs.com/package/blobshape)
---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import React, { useState } from "react";
+import "./App.scss";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <div className="App">
+      <h1 className="text-primary text-3xl"> Hi </h1>
+      <div className="w-32 h-32 bg-line-light">dsadasdasdsadas</div>
+    </div>
+  );
+}
+
+export default App;
