3d1ad8c765e04edc3cb7c4dbdf870b26d1e1e277
Filip Ivanovic <alkanoidev@gmail.com>
2022-06-11 17:09:41 +0200

    [feat] social links component

---
+++ src/components/SocialLink/SocialLink.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+type Props = {link: string; icon: JSX.Element;};
+
+export default function SocialLink({link, icon}: Props) {
+  return (
+    <a
+      href={link}
+      target="_blank"
+      className="text-subh text-bg-dark dark:text-bg transition-transform hover:scale-125"
+    >
+        {icon}
+    </a>
+  );
+}
