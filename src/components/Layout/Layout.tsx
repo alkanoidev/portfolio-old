12952cd8f7b25d4a5ed715775dc22ef702b6d9aa
Filip Ivanovic <alkanoidev@gmail.com>
2022-07-30 14:55:18 +0200

    theme color, scrollbar

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,8 +1,10 @@
-import React, { useState } from "react";
+import { useState } from "react";
 import Navbar from "../Navbar/Navbar";
 import "./style.scss";
 import NavbarMobile from "../NavbarMobile/NavbarMobile";
 import MenuAndThemeToggle from "../MenuAndThemeToggle/MenuAndThemeToggle";
+import { Helmet } from "react-helmet";
+import { useThemeContext } from "../../context/ThemeProvider";
 
 type Props = {
   children?: any;
@@ -10,9 +12,17 @@ type Props = {
 
 export default function Layout({ children }: Props) {
   const [menu, setMenu] = useState<boolean>(false);
+  const { theme } = useThemeContext();
 
   return (
     <div className="Layout">
+      <Helmet>
+        <meta
+          name={theme === "dark" ? "#121212" : "#f5f5f5"}
+          content="#4285f4"
+        />
+      </Helmet>
+
       <Navbar />
       <MenuAndThemeToggle menu={menu} setMenu={setMenu} />
       {menu && <NavbarMobile setMenu={setMenu} menu={menu} />}
