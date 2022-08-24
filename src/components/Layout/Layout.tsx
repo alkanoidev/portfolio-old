73b40d0945aa369b34a94d2abba5464d875ff71b
Filip Ivanovic <alkanoidev@gmail.com>
2022-08-24 16:10:03 +0200

    removed react helmet

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -3,7 +3,6 @@ import Navbar from "../Navbar/Navbar";
 import "./style.scss";
 import NavbarMobile from "../NavbarMobile/NavbarMobile";
 import MenuAndThemeToggle from "../MenuAndThemeToggle/MenuAndThemeToggle";
-import { Helmet } from "react-helmet";
 import { useThemeContext } from "../../context/ThemeProvider";
 
 type Props = {
@@ -16,13 +15,6 @@ export default function Layout({ children }: Props) {
 
   return (
     <div className="Layout">
-      <Helmet>
-        <meta
-          name="theme-color"
-          content={theme === "dark" ? "#121212" : "#f5f5f5"}
-        />
-      </Helmet>
-
       <Navbar />
       <MenuAndThemeToggle menu={menu} setMenu={setMenu} />
       {menu && <NavbarMobile setMenu={setMenu} menu={menu} />}
