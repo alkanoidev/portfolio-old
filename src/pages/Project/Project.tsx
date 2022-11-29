9795ba248dabeeab205a5f9ea541d0784177b2a7
Filip Ivanovic <alkanoidev@gmail.com>
2022-11-30 00:53:36 +0100

    skeleton on project page

--- src/pages/Project/Project.tsx
+++ src/pages/Project/Project.tsx
@@ -11,8 +11,10 @@ export default function Project() {
     created_at: "",
     topics: [""],
   });
+  const [loading, setLoading] = useState(false);
 
   useEffect(() => {
+    setLoading(true);
     const getReadme = async () => {
       const res = await fetch(
         `https://raw.githubusercontent.com/alkanoidev/${projectName}/master/README.md`
@@ -29,12 +31,42 @@ export default function Project() {
         created_at: data.created_at,
         topics: data.topics,
       });
+      setLoading(false);
     };
 
     getReadme();
     getRepositoryData();
+    setTimeout(() => {
+    }, 1000);
   }, []);
-
+  if (loading) {
+    return (
+      <div className="flex justify-center items-center flex-col mt-12 sm:mt-12">
+        <div className="max-w-[700px] w-full flex justify-center items-start flex-col">
+          <div className="w-1/2 h-5 dark:bg-bg/50 bg-bg-dark/50 rounded-md mb-5"></div>
+          <div className="w-full h-4 dark:bg-bg/40 bg-bg-dark/40 rounded-md mb-3"></div>
+          <div className="w-full h-4 dark:bg-bg/40 bg-bg-dark/40 rounded-md mb-3"></div>
+          <div className="w-full h-4 dark:bg-bg/40 bg-bg-dark/40 rounded-md mb-3"></div>
+          <div className="w-full h-[200px] dark:bg-bg/30 bg-bg-dark/30 rounded-md mb-4"></div>
+          <div className="w-1/3 h-5 dark:bg-bg/50 bg-bg-dark/50 rounded-md mb-5"></div>
+          <div className="w-full h-4 dark:bg-bg/40 bg-bg-dark/40 rounded-md mb-3"></div>
+          <div className="w-full h-4 dark:bg-bg/40 bg-bg-dark/40 rounded-md mb-3"></div>
+          <div className="w-full h-4 dark:bg-bg/40 bg-bg-dark/40 rounded-md mb-3"></div>
+          <div className="w-full h-4 dark:bg-bg/40 bg-bg-dark/40 rounded-md mb-3"></div>
+          <div className="w-full h-4 dark:bg-bg/40 bg-bg-dark/40 rounded-md mb-3"></div>
+          <div className="w-5/12 h-5 dark:bg-bg/50 bg-bg-dark/40 rounded-md mb-5"></div>
+          <div className="w-full h-4 dark:bg-bg/40 bg-bg-dark/40 rounded-md mb-3"></div>
+          <div className="w-full h-4 dark:bg-bg/40 bg-bg-dark/40 rounded-md mb-3"></div>
+          <div className="w-full h-4 dark:bg-bg/40 bg-bg-dark/40 rounded-md mb-3"></div>
+          <div className="w-full h-4 dark:bg-bg/40 bg-bg-dark/40 rounded-md mb-3"></div>
+          <div className="w-full h-4 dark:bg-bg/40 bg-bg-dark/40 rounded-md mb-3"></div>
+          <div className="w-full h-4 dark:bg-bg/40 bg-bg-dark/40 rounded-md mb-3"></div>
+          <div className="w-full h-4 dark:bg-bg/40 bg-bg-dark/40 rounded-md mb-3"></div>
+          <div className="w-full h-[200px] dark:bg-bg/30 bg-bg-dark/30 rounded-md mb-4"></div>
+        </div>
+      </div>
+    );
+  }
   return (
     <div className="flex justify-center flex-col items-center mt-12 sm:mt-0">
       <div className="flex max-w-[700px] flex-wrap gap-3 mt-12 mx-2 sm:mx-0">
